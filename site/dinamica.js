127155551384392f8c6ed9648e592459303fee34
Arthur Pereira da Silva <arthurpdasillva@gmail.com>
2020-10-10 21:05:30 -0300

    versão semi-final

---
+++ site/dinamica.js
@@ -0,0 +1,31 @@
+let textoF = document.getElementById('texto');
+
+function transform(x = 0) {    
+    switch (x) {
+        case 'c':
+            textoF.value = textoF.value.replace(textoF.value[0], textoF.value[0].toUpperCase());
+            break;
+    
+        case 'u':
+                textoF.value = textoF.value.toUpperCase();
+            break;
+        
+        case 'l':
+            textoF.value = textoF.value.toLowerCase();
+            break;
+        
+        case 'i':
+            textoF.value = textoF.value.split(' ').reverse().join(' ');
+            break;
+       
+        default:
+            let array = textoF.value.toLowerCase().split(' ');
+            for (let index = 0; index < array.length; index++) {
+                // Slice retorne o segundo elemento do array
+                array[index] = array[index][0].toUpperCase() + array[index].slice(1); 
+            }
+            textoF.value = array.join(' ');
+            break;
+    }
+    
+}
